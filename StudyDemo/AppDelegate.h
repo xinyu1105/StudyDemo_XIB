7833b206f49805dd77f37c726115a74865d7b8a5
xinyu1105 <754979828@qq.com>
2017-07-04 16:57:21 +0800

    Initial Commit

---
+++ StudyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyDemo
+//
+//  Created by pengjiaxin on 2017/7/4.
+//  Copyright © 2017年 pengjiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
